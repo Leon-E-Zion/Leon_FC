19acf82cd50779001028bd94532e3e683f749345
Leon-E-Zion <leonzion7@outlook.com>
2022-10-07 20:03:11 +0800

    file_tree_output
    file_tree_output

--- leon_info.py
+++ leon_info.py
@@ -7,5 +7,27 @@ def init_test():
 # info_mes(key,mes)
 def info_mes(key,mes):
 	print('--------\n{}:{}\n--------'.format(key,mes))
-	
-	
+
+# get dict_dirs
+def fc_0(str_,prefix):
+    k_0 = str_.find("'")
+    k_1 = str_.find("'",(k_0+1))
+    return 'value_type:'+str_[k_0+1:k_1]
+def get_dict_dirs(text, prefix='\t'):
+    for i in text.keys():
+        if isinstance(text[i], dict):
+            print(prefix + '+ ' + i)
+            get_dict_dirs(text[i], prefix + '|   ')  # 最后一个└
+        else:
+            print(prefix + '- ' + i+' '+fc_0(str(type(text[i])),prefix))
+
+
+if __name__=='__main__':
+    # 初始化测试集
+    a = {"a":{"a":1,"b":2,"c":3},"b":2,"c":3}
+    get_dict_dirs(a)
+
+ 
+
+ 
+
--- leon_os.py
+++ leon_os.py
@@ -2,17 +2,50 @@ import os
 import sys
 
 def leon_mk(path):
-	try:
-		if path.find('.') == -1:
-			os.makedirs(path)
-		else:
-			with open(path,mode="w",encoding="utf-8") as f:
-	   			pass
-	except:
-		pass
+    try:
+        if path.find('.') == -1:
+            os.makedirs(path)
+        else:
+            with open(path,mode="w",encoding="utf-8") as f:
+                pass
+    except:
+        pass
 
 # 基于现有项目路径加载深层路径
 def leon_path_in(path):
-	root = os.getcwd()
-	sys.path.append(os.path.join(root,path))
-	return os.path.join(root,path)
+    root = os.getcwd()
+    # 判断操作系统
+    if sys.platform == 'win32':
+        path = path.split(r'\\')
+    else:
+        path = path.split(r'/')
+    path_ = ''
+    for pt in path:
+        path_ = os.path.join(path_,pt)
+    path_ = os.path.join(root,path_)
+    sys.path.append(path_)
+    return path_
+
+# 生成指定文件夹的目录树 并写入保存到指定文件中
+def dfs_showdir(f,path, depth):
+    if depth == 0:
+        print("root:[" + path + "]")
+        f.write("root:[" + path + "]"+'\n')
+    for item in os.listdir(path):
+        if item in ['.git', '.idea', '__pycache__']:
+            continue
+        print("| " * depth + "+--" + item)
+        f.write("| " * depth + "+--" + item+'\n')
+        new_item = path + '/' + item
+        if os.path.isdir(new_item):
+            dfs_showdir(f,new_item, depth + 1)
+
+        
+def get_filetree(path):
+    with open(os.path.join(path,'Leon-file_tree.txt'),"w+") as f:    #设置文件对象
+        str_ = dfs_showdir(f,path, 0)
+        
+
+if __name__ == '__main__':
+    get_filetree(r'D:\Leon-Coding\Leon-PublicPlacesManage-System')
+
